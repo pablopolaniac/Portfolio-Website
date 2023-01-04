d36a1a949a9edf1f446b7d04ddfc90cc8552095b
pablo <polania.pablo1@gmail.com>
2023-01-03 18:26:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Personal-Website
+
+This is a personal website/Blog
+I like photography and thought of displaying my photos somewhere. This website has HTML/CSS and a few Javascript functions.
+
+
---
+++ animte_menu/app.js
@@ -0,0 +1,21 @@
+﻿const hamburgerToggle = document.querySelector("#hamburger-toggle");
+const navbar = document.querySelector("#navbar");
+const submenus = document.querySelectorAll(".submenu-trigger")
+
+
+
+function onHamburgerClick() {
+    if (!navbar.classList.contains('open')) {
+        navbar.classList.add("open")
+
+    } else {
+        navbar.classList.remove("open");
+    }
+}
+
+hamburgerToggle.addEventListener("click", onHamburgerClick)
+submenus.forEach(sub => {
+    sub.addEventListener("click", () => {
+        sub.classList.toggle("open")
+    })
+});
